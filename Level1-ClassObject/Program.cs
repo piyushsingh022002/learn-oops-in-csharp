20b7a8ee547c6af9875ff4036254a7378d39319c
Piyush Singh <b221037@iiit-bh.ac.in>
2025-06-12 12:58:43 +0530

    Level 1 is done

---
+++ Level1-ClassObject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Level1_ClassObject
+{
+    public class Person
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hii i am {Name}. My age is {Age}!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name = "Piyush Singh";
+            p1.Age = 22;
+            p1.Introduce();
+
+            Person p2 = new Person { Name = "Piyush Singh 2", Age = 23 };
+            p2.Introduce();
+        }
+    }
+}
